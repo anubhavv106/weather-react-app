1daf226df5874af8a51389e23bbe27c504561078
anubhavv016 <avdzav10@gmail.com>
2023-12-25 13:09:54 +0530

    new

---
+++ src/api/api.js
@@ -0,0 +1,11 @@
+const apiKey = 'b10e3157db5e47a8d61574798ee1022f';
+
+const getWeather = async (city) => {
+    return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+    .then((res) => res.json())
+    .then((json) => {
+        return json;
+    })
+}
+
+export default getWeather;
